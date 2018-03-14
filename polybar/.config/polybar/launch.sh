3b177cc8257d6dea8e4bc659ff19de2c0490446c
Thomas LECLERCQ <me@thomas.sh>
2018-03-14 10:57:53 +0530

    Delete useless comment + Add paris time

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -5,7 +5,8 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -x polybar >/dev/null; do sleep 1; done
 
-ls les doigts dans ke cuk
+# Launch bar1 and bar2
+
 
 if type "xrandr"; then
   for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
