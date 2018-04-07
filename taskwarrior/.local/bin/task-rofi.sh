57cce6ca50250ff942506f00470dc16b031f41cf
Thomas LECLERCQ <me@thomas.sh>
2018-04-07 17:18:17 +0530

    Add taskwarrior rofi

---
+++ taskwarrior/.local/bin/task-rofi.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+TASK=`task rc.verbose=nothing | rofi -dmenu -p "Task "`
+
+if [ -z "$TASK" ]; then
+    exit 0
+fi
+
+TASK_ID=`echo $TASK | cut -f1 -d' '`
+
+ACTION=`echo -e "start\nstop\ndone" | rofi -dmenu -p "Action"`
+
+if [ -n "$ACTION" ]; then
+    task $TASK_ID $ACTION
+fi
