f9751b393ec2709de5cb9cab37a90ed15486b255
Thomas LECLERCQ <me@thomas.sh>
2018-07-24 10:48:44 +0200

    tmux fix scratchpad and attempt at fixing lyvi crash

--- tmux/.config/tmux/cmus.sh
+++ tmux/.config/tmux/cmus.sh
@@ -4,7 +4,7 @@ tmux has -t 'cmus' || (
   tmux new-session -d -s'cmus'
   tmux send-keys "cmus" C-m
   tmux splitw -h -p 10
-  tmux send-keys "lyvi" C-m
+  tmux send-keys "until pgrep cmus;  do sleep 2; done; lyvi" C-m
   tmux selectp -t 1
 )
 
--- tmux/.config/tmux/scratchpad.sh
+++ tmux/.config/tmux/scratchpad.sh
@@ -2,5 +2,7 @@ tmux has -t 'scratchpad' || (
   tmux new-session -d -s 'scratchpad'
   tmux send-keys "htop" C-m
   tmux splitw -v -p 5 bc -ql
+  tmux splitw -h
+  tmux send-keys "cal -n 3 -m" C-m
 )
 
