9cb2e7e820f13aa7aee24350d3dcdd87d4525bfd
Thomas LECLERCQ <thomasle@mailbox.org>
2026-05-08 17:28:55 +0200

    Gopass wrapper update

--- gopass/.config/gopass/gopass_wrapper.sh
+++ gopass/.config/gopass/gopass_wrapper.sh
@@ -1,13 +1,21 @@
-#!/bin/bash
+#!/bin/sh
+
+export PATH="$PATH:$HOME/.nix-profile/bin" # required for Nix
+export PATH="$PATH:/usr/local/bin" # required on MacOS/brew
+export PATH="$PATH:/usr/local/MacGPG2/bin" # required on MacOS/GPGTools GPGSuite
+export GPG_TTY="$(tty)"
+# Uncomment to debug gopass-jsonapi
+# export GOPASS_DEBUG_LOG=/tmp/gopass-jsonapi.log
 
 if [ -f ~/.gpg-agent-info ] && [ -n "$(pgrep gpg-agent)" ]; then
-source ~/.gpg-agent-info
-export GPG_AGENT_INFO
+	source ~/.gpg-agent-info
+	export GPG_AGENT_INFO
 else
-eval $(gpg-agent --daemon)
+	eval $(gpg-agent --daemon)
 fi
 
-export PATH="$PATH:/usr/local/bin" # required on MacOS/brew
-export GPG_TTY="$(tty)"
-/usr/bin/gopass jsonapi listen
+export PATH="$PATH:/usr/local/bin"
+
+/usr/bin/gopass-jsonapi listen
+
 exit $?
